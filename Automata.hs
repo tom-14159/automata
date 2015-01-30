f1e3a711428fd0cbb6e5b7d65a003f4b4c9e02b7
Tomas <t.zabojnik@gmail.com>
2015-01-30 15:09:59 +0100

    BiAutomata

--- Automata.hs
+++ Automata.hs
@@ -13,27 +13,52 @@ type State t = t
 type Delta t = State t -> Char -> State t
 
 data FSM t = DFA (Set (State t), Alpha, Delta t, State t, Set (State t))
+	| BiDFA (Set (State t), Alpha, Delta t, Delta t, State t, Set (State t))
 
 escape :: String -> String
 escape s = "\"" ++ replace "\"" "\\\"" s ++ "\""
 
 gvtrans :: Show t => FSM t -> State t -> [String]
 gvtrans (DFA (s, al, d, i, f)) st = map (\a->"\t" ++ (escape.show) st ++ " -> " ++ (escape.show) (d st a) ++ " [ label = \""++[a]++"\" ];" ) (toList al)
+gvtrans (BiDFA (s, al, d, g, i, f)) st =
+	map (\a->"\t" ++ (escape.show) st ++ " -> " ++ (escape.show) (d st a) ++ " [ label=\""++[a]++"\" color=\"blue\" ];" ) (toList al)
+	++ map (\a->"\t" ++ (escape.show) st ++ " -> " ++ (escape.show) (g st a) ++ " [ label=\""++[a]++"\" color=\"red\" ];" ) (toList al)
+
+get_states :: FSM t -> Set (State t)
+get_states (DFA (s, al, d, i, f)) = s
+get_states (BiDFA (s, al, d, g, i, f)) = s
+
+get_final_states :: Show t => FSM t -> Set (State t)
+get_final_states (DFA (s, al, d, i, f)) = f
+get_final_states (BiDFA (s, al, d, g, i, f)) = f
 
 instance (Show t) => Show (FSM t) where
-	show dfa@(DFA (s, a, d, i, f)) = unlines $ [
+	show m = unlines $ [
 			"digraph finite_state_machine {",
 			"\trankdir=LR;",
 			"\tsize=\"8,5\";",
 			"\tnode [shape = doublecircle];",
-			"\t"++(join " " $ map (escape.show) $ toList f)++(if size f > 0 then ";" else ""),
+			"\t"++(join " " $ map (escape.show) $ toList $ get_final_states m)++(if size (get_final_states m) > 0 then ";" else ""),
 			"\tnode [shape = circle];"
-		] ++ concat (map (gvtrans dfa) (toList s)) ++ [
+		] ++ concat (map (gvtrans m) (toList $ get_states m)) ++ [
 			"}"
 		]
 
+dfa :: Ord (State t) => FSM t -> FSM t
+dfa m@(DFA (s, a, d, i, f)) = m
+dfa m@(BiDFA(s, a, d, g, i, f)) = DFA (s, a, d, i, f)
+
+bidfa :: Ord (State t) => FSM t -> FSM (t, Set t)
+bidfa (DFA (s, a, d, i, f)) =
+	BiDFA(new_states, a, new_delta, new_gamma, (i,f), S.filter (uncurry S.member) new_states)
+		where new_delta = \(q,p) x -> (d q x, p);
+			new_gamma = \(q,p) x -> (q, S.filter (\y -> S.member (d y x) p) s);
+			new_states = reachable_states (BiDFA (empty, a, new_delta, new_gamma, (i,f), empty)) (singleton (i,f)) [(i,f)]
+
+
 co :: Ord (State t) => FSM t -> FSM t
 co (DFA (s, a, d, i, f)) = DFA (s, a, d, i, s `difference` f)
+co (BiDFA (s, a, d, g, i, f)) = BiDFA (s, a, d, g, i, s `difference` f)
 
 empty_lang :: Alpha -> FSM Int
 empty_lang a = DFA (singleton 0, a, const, 0, empty)
@@ -44,21 +69,26 @@ reachable_states :: Ord (State t) =>
 	[t]	->	-- states to explore
 	Set t		-- reachable states
 reachable_states _ r [] = r
-reachable_states m@(DFA (s, a, d, i, f)) reached (h:t) =
+reachable_states m@(DFA (_, a, d, i, f)) reached (h:t) =
 	reachable_states m (reached `union` new_found) (toList(new_found `difference` reached) ++ t)
 		where new_found = S.map (d h) a
+reachable_states m@(BiDFA (_, a, d, g, _, _)) reached (h:t) =
+	reachable_states m (reached `union` new_found) (toList(new_found `difference` reached) ++ t)
+		where new_found = S.map (d h) a `union` S.map (g h) a
 
 reachable :: Ord (State t) => FSM t -> FSM  t
 reachable m@(DFA (s, a, d, i, f)) = DFA (r, a, d, i, f `intersection` r) where r = reachable_states m (singleton i) [i]
+reachable m@(BiDFA (s, a, d, g, i, f)) = BiDFA (r, a, d, g, i, f `intersection` r) where r = reachable_states m (singleton i) [i]
 
 product_set :: (Ord a, Ord b) => Set a -> Set b -> Set (a,b)
 product_set a b = fromList $ [(x,y) | x<-(toList a), y<-(toList b)]
 
 leads_to :: Ord t => FSM t -> State t -> [State t]
 leads_to m@(DFA (s, a, d, i, f)) st = map (d st) (toList a)
+leads_to m@(BiDFA (s, a, d, g, i, f)) st = map (d st) (toList a) ++ map (g st) (toList a)
 
 new_classes :: Ord t => FSM t -> [Set (State t)] -> [(State t, [Int])]
-new_classes m@(DFA (s, a, d, i, f)) partition = toList $ S.map (\st->(st, (get_index partition st : map (get_index partition) (leads_to m st)))) s
+new_classes m partition = toList $ S.map (\st->(st, (get_index partition st : map (get_index partition) (leads_to m st)))) (get_states m)
 	where get_index p e = case findIndex (member e) p of { Just x -> x }
 
 refine :: Ord t => FSM t -> [Set (State t)] -> [Set (State t)]
@@ -69,6 +99,9 @@ refine_many m p = if length p /= length r then refine_many m r else r where r = 
 
 minimize :: Ord t => FSM t -> FSM (Set (State t))
 minimize m@(DFA (s, a, d, i, f)) = DFA (new_states, a, (\st al->get_class new_states (d (findMin st) al)), get_class new_states i, S.map (get_class new_states) f)
+	where get_class p i = fold (\x y -> if i `member` x then x else y) empty p;
+		new_states = fromList $ refine_many m [s `difference` f, f]
+minimize m@(BiDFA (s, a, d, g, i, f)) = BiDFA (new_states, a, (\st al->get_class new_states (d (findMin st) al)), (\st al->get_class new_states (g (findMin st) al)), get_class new_states i, S.map (get_class new_states) f)
 	where get_class p i = fold (\x y -> if i `member` x then x else y) empty p;
 		new_states = fromList $ refine_many m [s `difference` f, f]
 
@@ -98,6 +131,9 @@ elem_index x (h:t) = if x==h then 1 else 1 + elem_index x t
 int_states :: (Ord (State s)) => FSM s -> FSM Int
 int_states (DFA (s, a, d, i, f)) =
 	DFA (fromList [1..size s], a, (\st al->get_index $ d (list_state!!(st-1)) al), get_index i, S.map get_index f)
+		where list_state = toList s; get_index = flip elem_index list_state
+int_states (BiDFA (s, a, d, g, i, f)) =
+	BiDFA (fromList [1..size s], a, (\st al->get_index $ d (list_state!!(st-1)) al), (\st al->get_index $ g (list_state!!(st-1)) al), get_index i, S.map get_index f)
 		where list_state = toList s; get_index = flip elem_index list_state
 
 ssw_delta :: Set String -> String -> Char -> String
